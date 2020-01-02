f5ffd917e1ccb1c39d3b23140773f31b708a82af
suraj bose <suraj.bose@au.ey.com>
2020-01-02 11:53:54 +1100

    Add project files.

---
+++ CoreWebApp.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using CoreWebApp.DataAccess.Data.Repository.IRepository;
+using CoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreWebApp.DataAccess.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetCategoryListForDropDown()
+        {
+            return _db.Category.Select(x=> new SelectListItem() { Text = x.Name, Value=x.Id.ToString() });
+        }
+
+        public void Update(Category category)
+        {
+            var dbCategory = _db.Category.FirstOrDefault(x => x.Id == category.Id);
+            dbCategory.Name = category.Name;
+            dbCategory.DisplayOrder = category.DisplayOrder;
+
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ CoreWebApp.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using CoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace CoreWebApp.DataAccess.Data.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(Category category);
+    }
+}
